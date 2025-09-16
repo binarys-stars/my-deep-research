b4c45bb50d6daa1797aedfc7d25ce27620d843ae
相其迪 <463556354@qq.com>
2025-09-16 18:03:29 +0800

    提交

--- first_agent/main.go
+++ first_agent/main.go
@@ -2,7 +2,16 @@ package main
 
 import (
 	"context"
+	"github.com/binarys-stars/my-deep-research.git/logs"
+	"github.com/cloudwego/eino-examples/flow/agent/deer-go/biz/infra"
+	"github.com/cloudwego/eino/components/model"
+	"github.com/cloudwego/eino/components/tool"
+	"github.com/cloudwego/eino/compose"
+	"github.com/cloudwego/eino/flow/agent"
+	"github.com/cloudwego/eino/flow/agent/react"
+	"github.com/cloudwego/eino/schema"
 	"log"
+
 )
 
 func main() {
@@ -25,7 +34,7 @@ func main() {
 	}
 
 	log.Printf("===bind tools to chat model===\n")
-	chatModel, err := chatModel.WithTools(toolInfos)
+	chatModel, err = chatModel.WithTools(toolInfos)
 	if err != nil {
 		logs.Errorf("bind tools to chat model failed, err=%v", err)
 		return
@@ -56,4 +65,88 @@ func main() {
 		return
 	}
 
+}
+
+func getTools() []tool.BaseTool {
+	tools := []tool.BaseTool{
+		GetDuckDuckGoTool(),
+		GetBrowserTool(),
+	}
+
+	return tools
+}
+
+func buildToolsInfo(ctx context.Context, tools []tool.BaseTool) []*schema.ToolInfo {
+	var toolInfos []*schema.ToolInfo
+	for _, t := range tools {
+		info, err := t.Info(ctx)
+		if err != nil {
+			logs.Errorf("get tool info failed, err=%v", err)
+			return nil
+		}
+		toolInfos = append(toolInfos, info)
+	}
+	return toolInfos
+}
+
+func buildToolsNode(ctx context.Context, tools []tool.BaseTool) *compose.ToolsNode {
+	toolsNode, err := compose.NewToolNode(ctx, &compose.ToolsNodeConfig{
+		Tools: tools,
+	})
+	if err != nil {
+		logs.Errorf("create tools node failed, err=%v", err)
+		return nil
+	}
+	return toolsNode
+}
+
+func runAgentByChain(ctx context.Context, chatModel model.ChatModel, tools *compose.ToolsNode, messages []*schema.Message) error {
+	// 通过Chain构建Agent
+	// 构建完整的处理链
+	chain := compose.NewChain[[]*schema.Message, []*schema.Message]()
+	chain.
+		AppendChatModel(chatModel, compose.WithNodeName("chat_model_1")).
+		AppendToolsNode(tools, compose.WithNodeName("search_tools"))
+
+	//编译并运行 chain
+	cagent, err := chain.Compile(ctx)
+	if err != nil {
+		return err
+	}
+
+	// 执行Agent
+	resp, err := cagent.Invoke(ctx, messages)
+
+	if err != nil {
+		return err
+	}
+
+	// 输出结果
+	for idx, msg := range resp {
+		logs.Infof("\n")
+		logs.Infof("message index: %d, role: %s", idx, msg.Role)
+		logs.Infof("content: %s", msg.Content)
+	}
+
+	return nil
+}
+
+func runAgentByReAct(ctx context.Context, chatModel model.ToolCallingChatModel, tools compose.ToolsNodeConfig, messages []*schema.Message) error {
+	ragent, err := react.NewAgent(ctx, &react.AgentConfig{
+		ToolCallingModel: chatModel,
+		ToolsConfig:      tools,
+	})
+	if err != nil {
+		return err
+	}
+	// 执行Agent
+	rmsg, err := ragent.Generate(ctx, messages, agent.WithComposeOptions(compose.WithCallbacks(&infra.LoggerCallback{})))
+	if err != nil {
+		return err
+	}
+	// 输出结果
+	logs.Infof("\n")
+	logs.Infof("message index: %d, role: %s, content: %s", 0, rmsg.Role, rmsg.Content)
+
+	return nil
 }
---
+++ first_agent/tools.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"github.com/binarys-stars/my-deep-research.git/logs"
+	"github.com/cloudwego/eino-ext/components/tool/duckduckgo/v2"
+	"github.com/cloudwego/eino/components/tool"
+	"github.com/cloudwego/eino/components/tool/utils"
+	"github.com/cloudwego/eino/schema"
+	"os/exec"
+	"runtime"
+)
+
+func GetDuckDuckGoTool() tool.InvokableTool {
+	ctx := context.Background()
+	// 创建 DuckDuckGo 工具
+	searchTool, err := duckduckgo.NewTextSearchTool(ctx, &duckduckgo.Config{})
+	if err != nil {
+		logs.Errorf("NewTextSearchTool failed, err=%v", err)
+		return nil
+	}
+	return searchTool
+}
+
+type BrowserParam struct {
+	Url string `json:"url"`
+}
+
+func OpenBrowser(_ context.Context, params *BrowserParam) (string, error) {
+	// 打开浏览器
+	logs.Infof("open browser, url=%s", params.Url)
+
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "linux":
+		cmd = exec.Command("xdg-open", params.Url)
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "start", "chrome", params.Url)
+	case "darwin":
+		cmd = exec.Command("open", params.Url)
+	default:
+		return "unsupported platform", nil
+	}
+	err := cmd.Run()
+	if err != nil {
+		logs.Errorf("open browser failed, err=%v", err)
+		return "open browser failed", err
+	}
+	return "open browser success", nil
+}
+func GetBrowserTool() tool.InvokableTool {
+	// 创建浏览器工具
+	info := &schema.ToolInfo{
+		Name: "open_browser",
+		Desc: "打开浏览器，参数是一个包含url字段的json对象，例如：{\"url\": \"https://www.google.com\"}。如果不支持打开浏览器，请返回错误信息",
+		ParamsOneOf: schema.NewParamsOneOfByParams(map[string]*schema.ParameterInfo{
+			"url": {
+				Type: "string",
+				Desc: "要打开的URL",
+			},
+		}),
+	}
+	return utils.NewTool(info, OpenBrowser)
+}
